98c0e29bda2877cd8db88cc7e3d08af541f6f567
Jonathan Andrew Hobern <u5011010@anu.edu.au>
2015-12-19 20:14:38 +1100

    Added in the initial files. The library can display integers, and latin letters, with inconsistent capitalization.

---
+++ FourDigitDisplay.cpp
@@ -0,0 +1,128 @@
+#include "FourDigitDisplay.h"
+#include <Arduino.h>
+
+FourDigitDisplay::FourDigitDisplay(const int digitPins[], const int segmentPins[])
+{
+	//copy all of the digit pins and segment pins to this object
+	for(int i = 0; i < num_digits_; i++)
+	{
+		digitPins_[i] = digitPins[i];
+	}
+
+	for(int i = 0;i < num_segments_; i++)
+	{
+		segmentPins_[i] = segmentPins[i];
+	}
+
+	memcpy(segmentPins_, segmentPins, num_segments_ * 2);
+
+	//set all of the digit pins to be OUTPUTS, and set them to HIGH, because HIGH == Off
+	for(int i=0;i<num_digits_;i++)
+	{
+		pinMode(digitPins_[i], OUTPUT);
+		digitalWrite(digitPins_[i], HIGH);
+	}
+
+	//initialise all of the segment pins as outputs
+	for(int i=0;i<num_segments_;i++)
+	{
+		pinMode(segmentPins_[i], OUTPUT);
+	}
+}
+
+void FourDigitDisplay::displayCharacter(const char& character, const char& position)
+{
+	//turn on the character in the correct position
+	for(int i=0;i<num_digits_;i++)
+	{
+		if(i != position) {
+			digitalWrite(digitPins_[i], HIGH);
+		} else {
+			digitalWrite(digitPins_[i], LOW);
+		}
+	}
+
+
+	//segments is used to point to the correct set of segments for displaying the character
+	const int* segments;
+
+	//this character is a digit
+	if(character >= '0' && character <='9')
+	{
+		char c = character - '0';
+		segments = digitSegments_[c];
+	} 
+	//this character is a lower case letter
+	else if(character >= 'a' && character <= 'z')
+	{
+		char c = character - 'a';
+		//currently, the same array is used for lower case and upper case letters
+		segments = characterSegments_[c];
+	}
+	//this character is an upper case letter
+	else if(character >= 'A' && character <= 'Z')
+	{
+		char c = character - 'A';
+		//currently, the same array is used for lower case and upper case letters
+		segments = characterSegments_[c];
+	}
+	//if we get to this point, the user has asked to display a character that has not been implemented
+	//so we just display an underscore, as a placeholder
+	else {
+		for(int i = 0; i < num_segments_; i++)
+		{
+			digitalWrite(segmentPins_[i], LOW);
+		}
+		//segmentPins_[3] is the lowest segment, an underscore
+		digitalWrite(segmentPins_[3], HIGH);
+	}
+
+	//loop through all of the segments in our character, to decide what to display
+	for(int i=0;i<num_segments_;i++)
+	{
+		//write HIGH if the segment is meant to be turned on for the character, low otherwise
+		if(segments[i] == 1) {
+			digitalWrite(segmentPins_[i],HIGH);
+		} else {
+			digitalWrite(segmentPins_[i],LOW);
+		}
+	}
+
+	//this delay is required in order to stop the characters "bleeding" into other positions
+	delay(5);
+}
+
+void FourDigitDisplay::displayDigit(const char& value, const char& position)
+{
+	//we need to add '0' to the digit in order to turn it into the appropriate character
+	displayCharacter(value + '0', position);
+}
+
+void FourDigitDisplay::displayInteger(const int& value)
+{
+	//the special case of k being 0 is handled seperately, so that a simpler algorithm
+	//can be used to draw all other numbers.
+	if(value == 0)
+	{
+		displayDigit(0, num_digits_ - 1);
+		return;
+	}
+	int k = value;
+
+	//display the digits in the segments from right to left.
+	char digit = num_digits_-1;
+	for(; digit>=0 && k!=0; k/=10, digit--)
+	{
+		displayDigit(k%10, digit);
+	}
+}
+
+void FourDigitDisplay::displayText(const char* text)
+{
+	//display characters until there are no more characters in the text,
+	//or we have run out of digits to use to display characters
+	for(char i = 0; text[i] != 0 && i < num_digits_; i++)
+	{
+		displayCharacter(text[i], i);
+	}
+}
---
+++ FourDigitDisplay.h
@@ -0,0 +1,104 @@
+#ifndef FOURDIGITDISPLAY_HPP
+#define FOURDIGITDISPLAY_HPP
+
+/*
+	Author	: Jonathan Andrew Hobern
+	Date	: 19/12/15
+	License	: GNU LESSER GENERAL PUBLIC LICENSE Version 3, 29 June 2007
+
+This library is used to power the SR420281K 7 segment (8 with decimal points), 4 digit displays.
+
+*/
+class FourDigitDisplay
+{
+public:
+	FourDigitDisplay(const int digitPins[], const int segmentPins[]);
+
+	/*
+		displayInteger displays the last num_digits_ digits from value onto the display
+		For example, the number abcdefg would be displayed as defg.
+		The numbers are right justified.
+	*/
+	void displayInteger(const int& value);
+
+	/*
+		displayText displays the first num_digits characters from a given array of text
+		onto the display.
+		For example, the text "abcdefg" would be displayed as abcd
+		The text is left justified.
+	*/
+	void displayText(const char* text);
+
+private:
+	//These constants represent the number of digits and segments in the display
+	static const int num_digits_ = 4;
+	static const int num_segments_ = 8;
+	
+	//two arrays that are used to store which pins drive the digits of the display,
+	//and which pins drive the segment. Each array is in order, i.e. the first element
+	//of digitPins_ drives the first digit of the display etc.
+	int digitPins_[num_digits_];
+	int segmentPins_[num_segments_];
+
+	/*
+		displayCharacter displays a single character at the given position on the display.
+	*/
+	void displayCharacter(const char& character, const char& position);
+
+	/*
+		displayDigit is used to display a single digit at a given position in the display
+	*/
+	void displayDigit(const char& character, const char& position);
+
+	//digitSegments_ stores the list of segments that should be turned on in order
+	//to display a particular digit.
+	//digitSegments_[n] is an array, with 1 in position m if segment m should be turned on
+	//in order to display digit n correctly, and a 0 in position m otherwise.
+	const int digitSegments_[10][num_segments_] = {
+		{1,1,1,1,1,1,0,0}, //0
+		{0,1,1,0,0,0,0,0}, //1
+		{1,1,0,1,1,0,1,0}, //2
+		{1,1,1,1,0,0,1,0}, //3
+		{0,1,1,0,0,1,1,0}, //4
+		{1,0,1,1,0,1,1,0}, //5
+		{1,0,1,1,1,1,1,0}, //6
+		{1,1,1,0,0,0,0,0}, //7
+		{1,1,1,1,1,1,1,0}, //8
+		{1,1,1,1,0,1,1,0}  //9
+	};
+
+	//characterSegments_ stores the list of segments that should be turned on in order
+	//to display a particular character.
+	//charactersSegments_[c-'a'] is an array, with 1 in position m if segment m should be turned on
+	//in order to display character c correctly, and a 0 in position m otherwise.
+	const int characterSegments_[26][num_segments_] = {
+		{1,0,1,0,1,1,1,0}, //A
+		{0,0,1,1,1,1,1,0}, //b
+		{1,0,0,1,1,1,0,0}, //C
+		{0,1,1,1,1,0,1,0}, //d
+		{1,1,0,1,1,1,1,0}, //e
+		{1,0,0,0,1,1,1,0}, //F
+		{1,1,1,1,0,1,1,0}, //g
+		{0,1,1,0,1,1,1,0}, //H
+		{0,0,0,0,1,1,0,0}, //I
+		{0,1,1,1,1,0,0,0}, //J
+		{1,0,1,0,1,1,1,0}, //k
+		{0,0,0,1,1,1,0,0}, //L
+		{1,0,1,0,1,0,0,0}, //m
+		{1,1,1,0,1,1,0,0}, //n
+		{0,0,1,1,1,0,1,0}, //o
+		{1,1,0,0,1,1,1,0}, //p
+		{1,1,1,0,0,1,1,1}, //q
+		{1,1,0,0,1,1,0,0}, //r
+		{1,0,1,1,0,1,1,0}, //S
+		{0,0,0,1,1,1,1,0}, //t
+		{0,0,1,1,1,0,0,1}, //u
+		{0,0,1,1,1,0,0,0}, //v
+		{0,1,0,1,0,1,0,0}, //w
+		{0,1,1,0,1,1,1,0}, //X
+		{0,1,1,1,0,1,1,0}, //y
+		{1,1,0,1,1,0,1,0}, //Z
+	};
+};
+
+#endif //FOURDIGITDISPLAY_HPP
